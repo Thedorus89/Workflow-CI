dd11d1487fd1e1bd84829a163a3e4a2e175c893c
Thedorus89 <theodoredanang@gmail.com>
2026-05-27 17:02:31 +0700

    first commit

---
+++ MLProject/modelling.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+import mlflow
+import mlflow.sklearn
+
+# untuk local testing
+# mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+mlflow.sklearn.autolog()
+
+df = pd.read_csv("dataset_preprocessing.csv")
+
+df = df.drop(columns=["Name", "Ticket"])
+
+
+X = df.drop("Survived", axis=1)
+y = df["Survived"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+
+with mlflow.start_run():
+
+    model = RandomForestClassifier(
+        n_estimators=100,
+        random_state=42
+    )
+
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    acc = accuracy_score(y_test, y_pred)
+
+    print("Accuracy:", acc)
+
+    mlflow.log_metric("accuracy", acc)
+
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model"
+    )
+
+print("Training selesai")
